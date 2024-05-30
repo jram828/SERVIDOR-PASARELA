46e7acbe6ee23c9a3d9d0b103a976340f8f5eb1f
jram828 <128335805+jram828@users.noreply.github.com>
2024-05-30 06:03:50 -0500

    original

--- server/server.js
+++ server/server.js
@@ -1,111 +1,101 @@
-import https from "https";
-// import  fs from 'fs';
-import cors from "cors";
-import { MercadoPagoConfig, Preference } from "mercadopago";
-import express from "express";
 
-import dotenv from "dotenv";
+import  https from 'https';
+import  fs from 'fs';
+import cors from 'cors'
+import { MercadoPagoConfig, Preference } from "mercadopago";
+import  express from "express";
+
+import  dotenv from "dotenv";
 dotenv.config();
 
-const client = new MercadoPagoConfig({
-  accessToken: process.env.ACCESS_TOKEN || "",
-});
+const client = new MercadoPagoConfig({accessToken: process.env.ACCESS_TOKEN || ""})
 
-// const options = {
-//   key: fs.readFileSync('./localhost-key.pem'), // Reemplaza con la ruta de tu llave generada
-//   cert: fs.readFileSync('./localhost.pem') // Reemplaza con la ruta de tu certificado generado
-// }
+const options = {
+  key: fs.readFileSync('./localhost-key.pem'), // Reemplaza con la ruta de tu llave generada
+  cert: fs.readFileSync('./localhost.pem') // Reemplaza con la ruta de tu certificado generado
+}
+
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-// app.use((req, res, next) => {
-//   console.log("Estableciendo politica de referencia");
-//   res.setHeader("Referrer-Policy", "strict-origin-when-cross-origin");
-//   next();
-// });
+app.use((req, res, next) => {
+  console.log('Estableciendo politica de referencia')
+  res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
+  next();
+});
 
 app.use(cors());
 app.use(express.json());
 
 app.post("/create_preference", async (req, res) => {
-  console.log("Estoy en crear preferencia por https!!!");
-  console.log("Mail del pagador...  ", req.body.payer);
+  console.log('Estoy en crear preferencia por https!!!')
+  console.log('Mail del pagador...  ',req.body.payer)
   const producto = req.body;
 
   try {
-    // Crear un objeto de preferencia
-    let body = {
-      items: [
+  
+// Crear un objeto de preferencia
+let body = {
+  items: [
         {
+
           title: req.body.title,
-          description: req.body.description,
           quantity: Number(req.body.quantity),
           unit_price: Number(req.body.price),
-          id: req.body.id,
-          currency_id: "COP",
+          currency_id: "ARS",
+          
         },
       ],
       payer: {
-        // email: req.body.payer,
-        email: "test_user_997292459@testuser.com",
+        email: req.body.payer
+        //email: 'test_user_1490493949@testuser.com',
       },
       payment_methods: {
-        excluded_payment_methods: [],
-        excluded_payment_types: [],
-        installments: 12,
+        // excluded_payment_types: [
+        //   {
+        //     id: 'ticket', // Excluir métodos de pago no deseados
+        //   },
+        // ],
+        installments: 1,
       },
       back_urls: {
-        success:
-          "https://proyecto-final-develop.vercel.app/#/home/payments/status",
-        failure: "https://proyecto-final-develop.vercel.app/#/home",
-        pending: "https://proyecto-final-develop.vercel.app/#/home",
+        success: "http://www.youtube.com/",
+        failure: "http://www.youtube.com",
       },
-      notification_url: "https://legaltech-develop.onrender.com/pagos/webhook",
+
       auto_return: "approved",
-    };
-
-    const preference = new Preference(client);
-    const result = await preference.create({ body });
-    console.log(result);
+    }
+    
+    const preference= new Preference(client)
+    const result = await preference.create({body});
+    console.log(result)
     // {redirectUrl: preference.init_point}
-    console.log(
-      "Esto se devuelve al front.......  ",
-      result.sandbox_init_point
-    );
-
-    res.status(200).json({ redirectUrl: result.sandbox_init_point });
-    // res.status(200).json({id:result.id});
+    console.log('Esto se devuelve al front.......  ',result.sandbox_init_point)
+    
+    res.status(200).json({redirectUrl:result.sandbox_init_point});
+   // res.status(200).json({id:result.id});
   } catch (error) {
     console.error(error.message);
     res.status(500).json(error.message);
   }
 });
 
+  
+  
+  
 app.use((req, res, next) => {
-  res.send("<h1>HTTPS Works!</h1>");
+  res.send('<h1>HTTPS Works!</h1>');
 });
 
-// https.createServer(options, app).listen(port, () => {
-//   console.log("Server listening on port " + port);
-// });
-
-// const express = require("express");
-// const router = require("./routes");
-// const morgan = require("morgan");
-// const cors = require("cors");
-
-// const server = express();
-
-// server.use(morgan("dev"));
-// server.use(express.json());
-// server.use(cors());
-
-// server.use(router);
-
-// module.exports = server;
-
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+https.createServer(options, app).listen(port,() => {
+  console.log('Server listening on port ' + port);
 });
+
+
+
+
+
+
+
